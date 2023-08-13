73e6b7197199665d988ef39f693c596e5208ef68
Preeti bhardwaj <127471577+Preeti684-bhardwaj@users.noreply.github.com>
2023-08-14 00:01:36 +0530

    Update action.js

--- src/component/redux/actions/action.js
+++ src/component/redux/actions/action.js
@@ -1,18 +1,35 @@
 export const getNews = (category) => async (dispatch) => {
-    try {
-      const host = 'https://quilted-ludicrous-rotate.glitch.me';
-      const data = await fetch(`${host}/news/getnews?category=${category}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-  
-      const res = await data.json();
-      console.log(res);
-      dispatch({ type: "SUCCESS_GET_NEWS", payload: res });
-    } catch (error) {
-      dispatch({ type: "FAIL_GET_NEWS", payload: error.response });
-    }
-  };
-  
+  try {
+    const host = 'http://localhost:5000';
+    const data = await fetch(`${host}/news/getnews?category=${category}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const res = await data.json();
+    console.log(res);
+    dispatch({ type: "SUCCESS_GET_NEWS", payload: res });
+  } catch (error) {
+    dispatch({ type: "FAIL_GET_NEWS", payload: error.response });
+  }
+};
+
+export const getAllNews = () => async (dispatch) => {
+  try {
+    const host = 'http://localhost:5000';
+    const data = await fetch(`${host}/news/getallnews`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const res = await data.json();
+    console.log(res);
+    dispatch({ type: "SUCCESS_GET_NEWS", payload: res });
+  } catch (error) {
+    dispatch({ type: "FAIL_GET_NEWS", payload: error.response });
+  }
+};
