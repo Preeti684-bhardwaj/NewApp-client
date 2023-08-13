363c61c91669d4ddce0c221f52f0282448021de4
Preeti bhardwaj <127471577+Preeti684-bhardwaj@users.noreply.github.com>
2023-08-13 21:50:26 +0530

    Update Home.jsx

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = (props) => {
   return (
     <div className="container" style={{color:props.mode==='dark'?'white':'black', textAlign: 'center',
-    margin: '100px auto',backgroundColor:'grey',padding:'20px',borderRadius:'20px'}}>
+    margin: '100px auto',backgroundColor:'grey',padding:'20px',borderRadius:'20px', width: '85vw'}}>
       <h1>Welcome to Daily News!</h1>
       <h3>This is your go-to source for the latest news.</h3>
       <h3>Choose a Category to get started with your daily dose of news</h3>
