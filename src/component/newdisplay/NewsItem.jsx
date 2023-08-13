fb4d636de0d77e0e198b18ceeba3b46c4a222880
Preeti bhardwaj <127471577+Preeti684-bhardwaj@users.noreply.github.com>
2023-08-14 03:09:19 +0530

    Update NewsItem.jsx

--- src/component/newdisplay/NewsItem.jsx
+++ src/component/newdisplay/NewsItem.jsx
@@ -15,7 +15,7 @@ const NewsItem = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text" >{description}...</p>
-                    <p className="card-text"><small className="text-body-secondary"> Published at {new Date(date).toGMTString()} at location {place}</small></p>
+                    <p className="card-text"><small className="text-body-secondary"> Published at {new Date(date).toGMTString()} in location {place}</small></p>
                     <div className="bt" style={{marginLeft:'20px'}}>
                     <NavLink to={`/news/getnews/${id}`} className="btn btn-sm btn-dark">
                         Read More
